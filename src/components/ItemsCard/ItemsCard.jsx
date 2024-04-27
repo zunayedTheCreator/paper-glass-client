0375b1ba52a8b3209fd043967df5a99b2d5a1d20
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-04-27 22:32:54 +0600

    craft items section added

---
+++ src/components/ItemsCard/ItemsCard.jsx
@@ -0,0 +1,30 @@
+import { FaDollarSign } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaRegClock } from "react-icons/fa";
+
+const ItemsCard = ({item}) => {
+
+    const {item_name, subcategory, price, rating, process_time, description, customization, stock_state, user_name, user_email, photo} = item;
+
+    return (
+        <div className="card card-side bg-base-100 w-[600px] h-[320px] shadow-xl rounded border text-black item">
+        <figure className="w-2/5 bg-black"><img src={photo} alt="Movie"/></figure>
+        <div className="card-body gap-0 w-3/5 bg-orange-300">
+            <div className="flex items-center justify-between">
+                <h2 className="card-title font-bold mb-1">{item_name}</h2>
+                <button className=" font-bold border-2 border-teal-700 px-2 rounded hover:bg-teal-400 hover:text-white duration-200">View</button>
+            </div>
+            <div className="border-t-2 border-black mb-3"></div>
+            <h5 className="font-bold flex items-center border-b-2 border-r-2 border-black py-1"><FaDollarSign className="text-lg"></FaDollarSign> {price}</h5>
+            <h5 className="font-bold flex items-center gap-1 border-b-2 border-r-2 border-black py-1"><FaStar className="text-lg"></FaStar> {rating}</h5>
+            <h5 className="font-bold flex items-center gap-1 border-b-2 border-r-2 border-black py-1"><FaRegClock className="text-lg"></FaRegClock> {process_time}</h5>
+            <h4 className="font-bold mt-4 mb-2">Email: {user_email}</h4>
+            <div className="card-actions justify-between items-center">
+                <h3 className="font-bold border-2 border-b-black border-r-black border-l-teal-400 border-t-teal-400 px-2 py-1 rounded">{subcategory}</h3>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default ItemsCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
+import ItemsCard from "../../components/ItemsCard/ItemsCard";
 
 const Home = () => {
+
+    const items = useLoaderData();
+
     return (
         <div>
             <Banner></Banner>
+            <div>
+                <h1 className="text-3xl font-bold text-center mt-8 mb-2">Craft Items</h1>
+                <div className="border-t-2 w-2/6 border-black mx-auto mb-8"></div>
+                <div className="w-full xl:w-[1300px] mx-auto grid grid-cols-1 xl:grid-cols-2 px-10 gap-5">
+                    {
+                        items.map(item => <ItemsCard item={item} key={item._id}></ItemsCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
